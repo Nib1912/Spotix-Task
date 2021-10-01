0c3ed8c43b59c6167071912cd30686d908e1114c
Nived Rajan <nivedrajan007@gmail.com>
2021-10-01 18:43:17 +0530

    completed UI

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffb3bcfc),
       body: Column(
         //crossAxisAlignment: CrossAxisAlignment.center,
         children: [
@@ -16,7 +18,7 @@ class ProfileScreen extends StatelessWidget {
                   size: 50,
                   color: Colors.white,
                 ),
-                margin: EdgeInsets.only(top: 50),
+                margin: EdgeInsets.only(top: 60),
                 padding: EdgeInsets.all(2.5),
                 decoration: BoxDecoration(
                   color: Colors.black,
@@ -26,8 +28,10 @@ class ProfileScreen extends StatelessWidget {
                 ),
               ),
               Container(
-                padding: EdgeInsets.only(left: 6,),
-                margin: EdgeInsets.only(top: 30),
+                padding: EdgeInsets.only(
+                  left: 6,
+                ),
+                margin: EdgeInsets.only(top: 35),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -39,20 +43,17 @@ class ProfileScreen extends StatelessWidget {
                           fontFamily: 'Palanquin',
                           fontSize: 30,
                           fontWeight: FontWeight.w800,
-
                         ),
                       ),
                     ),
                     Container(
-                      padding: EdgeInsets.symmetric(horizontal: 5,vertical: 2),
+                      padding: EdgeInsets.symmetric(horizontal: 5, vertical: 2),
                       child: Text(
                         'SPOT IT AND MAKE IT',
                         style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 8,
-                          fontFamily: 'Palanquin'
-
-                        ),
+                            color: Colors.white,
+                            fontSize: 8,
+                            fontFamily: 'Palanquin'),
                       ),
                       color: Colors.black,
                     ),
@@ -60,7 +61,40 @@ class ProfileScreen extends StatelessWidget {
                 ),
               )
             ],
-          )
+          ),
+          SizedBox(
+            height: 100,
+          ),
+          CircleAvatar(
+            radius: 100,
+            backgroundImage: AssetImage('images/Nived.jpeg'),
+          ),
+          SizedBox(height: 20),
+          Container(
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+                color: Color(0xff97a3fc),
+                borderRadius: BorderRadius.all(Radius.circular(15),),),
+            child: Text(
+              ' Nived Rajan',
+              style: TextStyle(
+                  fontFamily: 'Palanquin',
+                  fontSize: 17,
+                  color: Colors.white),
+            ),
+          ),
+          SizedBox(height: 10,),
+          Container(
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+                color: Color(0xff97a3fc),
+              borderRadius: BorderRadius.all(Radius.circular(15),),),
+            child: Text(
+              'Place: Kannur',
+              style: TextStyle(
+                  fontFamily: 'Palanquin', fontSize: 17, color: Colors.white),
+            ),
+          ),
         ],
       ),
     );
