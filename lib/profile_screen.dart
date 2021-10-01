db8992613aedd2d1487efb7d20df40af64fc6cd1
Nived Rajan <nivedrajan007@gmail.com>
2021-10-02 00:05:57 +0530

    animation implemented

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'logo.dart';
@@ -5,36 +6,50 @@ import 'reusable_container.dart';
 
 class ProfileScreen extends StatelessWidget {
   @override
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffb3bcfc),
-      body: ListView(
-        children:[
-          Column(
-            //crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Logo(),
-              SizedBox(
-                height: 100,
+      body: ListView(children: [
+        Column(
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Logo(),
+            SizedBox(
+              height: 100,
+            ),
+            CircleAvatar(
+              radius: 100,
+              backgroundImage: AssetImage('images/Nived.jpeg'),
+            ),
+            SizedBox(height: 20),
+            AnimationConfiguration.staggeredList(
+              position: 1,
+              duration: Duration(seconds: 1),
+              child: SlideAnimation(
+                child: ReusableContainer(
+                  label: 'Name :\t\t Nived Rajan',
+                ),
               ),
-              CircleAvatar(
-                radius: 100,
-                backgroundImage: AssetImage('images/Nived.jpeg'),
+            ),
+            AnimationConfiguration.staggeredList(
+              position: 1,
+              duration: Duration(seconds: 1),
+              child: SlideAnimation(
+                child: ReusableContainer(label: 'DOB :\t\t 16/04/1999'),
               ),
-              SizedBox(height: 20),
-              ReusableContainer(label: 'Name :\t\t Nived Rajan'),
-              ReusableContainer(label: 'DOB :\t\t 16/04/1999'),
-              ReusableContainer(label: 'Place :\t\t Kannur'),
-
-
-
-
-
-            ],
-          ),
-        ]
-      ),
+            ),
+            AnimationConfiguration.staggeredList(
+              position: 1,
+              duration: Duration(seconds: 1),
+              child: SlideAnimation(
+                child: ReusableContainer(label: 'Place :\t\t Kannur'),
+              ),
+            ),
+          ],
+        ),
+      ]),
     );
   }
 }
-
